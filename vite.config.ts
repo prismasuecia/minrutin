9634c001e7cc6e27756ab6457f219c45003d0204
Joel Lindhe <info@joellindhe.com>
2025-12-28 00:09:00 +0100

    PWA: add update prompt + auto reload; disable auto register injection

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      injectRegister: 'null',
       registerType: 'autoUpdate',
       includeAssets: ['**/*.png', '**/*.svg'],
       manifest: {
